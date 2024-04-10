b88eba8de3b1561bfcd655963675cc21df9f6abe
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-10 04:14:59 +0000

    moved html for homepage to elephant.erb

--- dice.rb
+++ dice.rb
@@ -11,15 +11,7 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 
 get("/") do
-  "<h1>Dice Roll App</h1>
-  <p>Roll some dice by visiting the following routes:</p>
-  <ul>
-    <li> <a href='/dice/2/6'>Roll two 6-sided dice</a> </li>
-    <li> <a href='/dice/2/10'>Roll two 10-sided dice</a> </li>
-    <li> <a href='/dice/1/20'>Roll one 20-sided die</a> </li>
-    <li> <a href='/dice/5/4'>Roll five 4-sided dice</a> </li>
-    <li> <a href='/dice/1000/6'>Roll one thousand 6-sided dice</a> </li>
-  </ul>"
+  erb(:elephant)
 end
 
 get("/zebra") do
