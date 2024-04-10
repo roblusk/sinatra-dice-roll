e5f10d8147ff87d2528fbf04fba7eec28de22cc5
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-10 03:53:18 +0000

    Added 6 and 10 sided dice

--- dice.rb
+++ dice.rb
@@ -2,9 +2,36 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "Hello World"
+  "Dice Roll App"
 end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
 end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end
+
+get("dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+  
+    "<h1>2d10</h1>
+    <p>#{outcome}</p>"
+end
+
